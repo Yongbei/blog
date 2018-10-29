290e04e4f468fc7b02e2800324a6cd6f7d458059
vagrant <vagrant@homestead>
2018-10-29 02:13:12 +0000

    add admin page and use webpack.mix.js and npm

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,23 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.styles([
+	'resources/sass/libs/bootstrap.css',
+	'resources/sass/libs/blog-post.css',
+	'resources/sass/libs/font-awesome.css',
+	'resources/sass/libs/metisMenu.css',
+	'resources/sass/libs/sb-admin-2.css',
+	'resources/sass/libs/styles.css',
+
+], 'public/css/libs.css');
+
+mix.scripts([
+	'resources/js/libs/bootstrap.js',
+	'resources/js/libs/jquery.js',
+	'resources/js/libs/metisMenu.js',
+	'resources/js/libs/sb-admin-2.js',
+	'resources/js/libs/script.js',
+
+], 'public/js/libs.js');
